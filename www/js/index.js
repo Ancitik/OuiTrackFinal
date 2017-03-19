2d7ab80b20ca8cfd8fcae6c11b16c9529e1ff890
Alexandre LAURANS <alexandre.laurans14@gmail.com>
2017-03-19 11:06:16 +0100

    Release v1.0

--- www/js/index.js
+++ www/js/index.js
@@ -1,26 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 'use strict';
 
 const ouibus_token = 'W9JdZqSu8EAzM5XSf8fCgw';
 
+//on définit ici toutes nos fonctions utiles dans une "boîte à outils"
 var outils = {
+        //renvoi une promesse qui résoud l'identifiant d'une ville passé en paramètres, en nom de ville
         getNomVilleParID: function(iden){
             // On renvoie une promesse qui prend en paramettre une fonction
             // avec 2 paramètres, le callback de succès et d'erreur
@@ -49,6 +33,7 @@ var outils = {
               });
         },
 
+         //renvoi une promesse qui résoud l'identifiant d'une ville passé en paramètres, en temps de la ville
         getTempsOfCity: function (city){
             return new Promise(function (resolve, reject) {
                 console.log(city);
@@ -78,6 +63,7 @@ var outils = {
             });
         },
 
+        //renvoi une promesse qui résoud l'identifiant d'une ville passé en paramètres, en température de la ville
         getTemperatureOfCity: function (city){
             return new Promise(function (resolve, reject) {
                 console.log(city);
@@ -116,50 +102,49 @@ var outils = {
         executeAsyncFunc2: function executeAsyncFunc2(d, city) {
             outils.getTempsOfCity(city).then(function(result){
                 console.log('executeAsyncFunc2 ' + result);
-                document.getElementById(d).textContent = result;
+                document.getElementById(d).textContent = "Temps : " + result;
             });
 		},
 
         executeAsyncFunc3: function executeAsyncFunc3(d, city) {
             outils.getTemperatureOfCity(city).then(function(result){
                 console.log('executeAsyncFunc3 ' + result);
-                document.getElementById(d).textContent = result;
+                document.getElementById(d).textContent = "Température : " + Math.round(result) + "°C";
             });
 		},
 
+        //fonction qui permet de ne pas afficher 2 fois le même trajet (l'APi fournit un même numéro de bus pour un "Paris-Nice" que pour un "Paris-Lyon-Nice")
         comparerTab: function (numBus, tab) {
-            //FIXME: effacement du mauvais doublon
-        var i;
-        var longueurTab = tab.length;
-        for (i = 0; i < longueurTab; i++) {
-            if (tab[i] == numBus) {
-                return false;
+            var i;
+            var longueurTab = tab.length;
+            for (i = 0; i < longueurTab; i++) {
+                if (tab[i] == numBus) {
+                    return false;
+                }
             }
-        }
-        return true;
+            return true;
     },
 
+        //fonction qui éfface le tableau de la page 3 
+        //on l'appelle quand on veut chercher un autre trajet (bouton de la page4) et quand on fait "backbutton" sur la page3
         effacerTableau: function () {
-        var i = 0;
+            var i = 0;
+            var lignes = document.getElementById("table").rows;
+            var nbLignes = lignes.length - 1;
+            console.log(lignes);
+            console.log(nbLignes);
 
-        var lignes = document.getElementById("table").rows;
-        var nbLignes = lignes.length - 1;
-        console.log(lignes);
-        console.log(nbLignes);
-
-        if (nbLignes > 1) {
-            //a l'envers
-            for (i = nbLignes; i > 0; i--) {
-                document.getElementById("table").deleteRow(i);
+            if (nbLignes > 1) {
+                //a l'envers
+                for (i = nbLignes; i > 0; i--) {
+                    document.getElementById("table").deleteRow(i);
+                }
             }
-        }
-        //refresh page 1 et redirection
+            //refresh page 1 et redirection
+            $.mobile.changePage('#page1');
+            location.reload();
 
-
-        $.mobile.changePage('#page1');
-        location.reload();
-
-    },
+        },
 
 
 };
@@ -172,17 +157,18 @@ var app = {
 
     //Deviceready Event Handler
     onDeviceReady: function() {
-        //TODO: tester connexion wifi ou 3g
         document.addEventListener("online", this.onOnline, false);
-        document.addEventListener("offline", this.onOffline, false)
+        document.addEventListener("offline", this.onOffline, false);
+        document.addEventListener("backbutton",this.onBackButton,false);
+        
 
         //lancer ici le spinner 1
         var options = { dimBackground: true };
         SpinnerPlugin.activityStart("Chargement...", options);
-
         document.getElementById('btnRechercherAvecNum').addEventListener('click', this.recupererInfoBillet.bind(this), false);
         document.getElementById('btnRechercherAvecVilles').addEventListener('click', this.recupererTrajets.bind(this), false);
         document.getElementById('btnRechercherNouveauTrajet').addEventListener('click', outils.effacerTableau.bind(this), false);
+        
 
         /*Récupérer les villes dans les selects*/
         var selectVilleDepart = document.getElementById('selectVilleDepart');
@@ -196,6 +182,7 @@ var app = {
                 'Authorization': ouibus_token ? `Token ${ouibus_token}` : null,
                 'Content-Type': 'application/json'
             },
+            //ici on va remplir les deux listes déroulantes de le page2
             success: function (result) {
                 var i = 0;
                 for (i = 0; i < result.stops.length; i++){
@@ -210,7 +197,7 @@ var app = {
                     option.value = result.stops[i].id;
                     selectVilleArrivee.add(option);
                 }
-                //arreter spinner 1
+                //arreter spinner
                 SpinnerPlugin.activityStop();
             },
             error: function (xhr, textStatus, errorThrown) {
@@ -219,11 +206,12 @@ var app = {
         });
     },
 
-    //Click btnRechercherAvecNum Event Handler
+    //La fonction qui se lance quand une recherche par numéro de bus et date
     recupererInfoBillet: function () {
+        //on récupère les inputs de l'utilisateur
         var numBus = $('#txtNumBus').val();
         var dateBus = $('#dateDepart1').val();
-
+        //on test si les champs sont remplis
         if (numBus === '' || dateBus === ''){
             alert('Remplissez tous les champs !');
             return;
@@ -274,28 +262,31 @@ var app = {
                             var villeDepartPage4 = result;
                             outils.executeAsyncFunc2('labelTempsArrivee', villeDepartPage4);
                             outils.executeAsyncFunc3('labelTemperatureArrivee', villeDepartPage4);
+                            SpinnerPlugin.activityStop();
                         });
 
-                        //Afficher le DOM de la page 4
+                        //Afficher la page 4
                         $.mobile.changePage('#page4');
                         //On arrête dès qu'on a trouvé
                         break;
                     }
-                    if (i == result.fares.length){
-                        //Numero non trouvé
+                    //message d'erreur si trajet n'a pas été trouvé
+                    if (i == result.fares.length-1){
                         alert('Aucun billet disponible pour ce numero de bus');
                     }
+                    SpinnerPlugin.activityStop();
                 }
+                
             },
             error: function (xhr, textStatus, errorThrown) {
-                console.log('Error: ' + textStatus + ' ' + errorThrown);
+                alert('Error: ' + textStatus + ' ' + errorThrown);
             }
         });
 
-        SpinnerPlugin.activityStop();
+        
     },
 
-    //Click btnRechercherAvecVilles Event Handler
+    //La fonction qui se lance quand une recherche par villes et date
     recupererTrajets: function () {
         var villeDepart = $('#selectVilleDepart').val();
         var villeArrivee = $('#selectVilleArrivee').val();
@@ -323,46 +314,42 @@ var app = {
 
                 if (result.fares.length === 0) {
                     alert('Aucun billet disponible pour ce trajet');
+                    SpinnerPlugin.activityStop();
                 } else {
                     var i;
                     var j;
                     var tab = [];
                     var ligneActuelle = 1;
-
+                    //pour chaque trajet trouvé
                     for (i = 0; i < result.fares.length; i++) {
+                        //si le trajet est encore valable (pas dépassé)
                         if (result.fares[i].available === true) {
+                            // et si le numéro de bus n'est pas en doublon
                             if (outils.comparerTab(result.fares[i].legs[0].bus_number, tab) === true) {
 
                                 var table = document.getElementById('table');
                                 var ligne = table.insertRow(ligneActuelle);
                                 ligneActuelle++;
-
+                                //on rajoute une ligne au tableau de la page3
                                 var num = ligne.insertCell(0);
                                 var dep = ligne.insertCell(1);
                                 var arr = ligne.insertCell(2);
                                 var date = ligne.insertCell(3);
-                                var etape = ligne.insertCell(4);
 
                                 tab.push(result.fares[i].legs[0].bus_number);
-                                console.log(tab);
-
                                 dep.innerHTML = result.fares[i].origin_id;
                                 num.innerHTML = result.fares[i].legs[0].bus_number;
                                 arr.innerHTML = result.fares[i].destination_id;
-                                date.innerHTML = result.fares[i].departure; //TODO : convertir en heure
+                                date.innerHTML = result.fares[i].departure;
 
+                                //on affecte au numéro un lien pour accéder à la page 4
                                 num.innerHTML = '<a href="#page4">' + result.fares[i].legs[0].bus_number + '</a>';
 
-                                //Invoke executeAsyncFunc to edit dep & arr innerHTML
+                                //Exécuter la requete executeAsyncFunc pour éditer le DOM de la page 4
                                 outils.executeAsyncFunc(dep, result.fares[i].origin_id);
                                 outils.executeAsyncFunc(arr, result.fares[i].destination_id);
-
-                                var etapes = '';
-                                for (j = 0; j < result.fares[i].legs.length; j++) {
-                                    etapes = etapes + result.fares[i].legs[j].destination_id + "-";
-                                }
-                                etape.innerHTML = etapes;
-
+                                
+                                //on fait un travail de découpage sur la date récupéré
                                 var heureDep = result.fares[i].departure;
                                 var heureDepSplit = heureDep.split("T");
                                 var heureDepSplit2 = heureDepSplit[1];
@@ -372,8 +359,6 @@ var app = {
                         }
                     }
                     //ville depart
-                    console.log(villeDepart);
-
                     outils.getNomVilleParID(villeDepart).then(function(result){
                         var villeDepartPage4 = result;
                         console.log("villeDepPage4" + villeDepartPage4);
@@ -383,20 +368,15 @@ var app = {
                         document.getElementById("labelDateDepart").textContent = dateDepart;
                     });
 
-                    //document.getElementById("labelVilleDepart").textContent = villeDepart;
-
-
                     //ville arrivée
-
                     outils.getNomVilleParID(villeArrivee).then(function(result){
                         var villeArriveePage4 = result;
                         document.getElementById('labelVilleArrivee').textContent = villeArriveePage4;
                         outils.executeAsyncFunc2('labelTempsArrivee', villeArriveePage4);
                         outils.executeAsyncFunc3('labelTemperatureArrivee', villeArriveePage4);
-                        //FIXME: recuperer date arrivee
                         document.getElementById("labelDateArrivee").textContent = dateDepart;
+                        SpinnerPlugin.activityStop();
                     });
-
 
                     $.mobile.changePage('#page3');
                 }
@@ -405,8 +385,7 @@ var app = {
                 console.log('Error: ' + textStatus + ' ' + errorThrown);
             }
         });
-        //FIXME: arreter spinner  quand la page est affiché
-        SpinnerPlugin.activityStop();
+        
     },
 
     //onOnline Event Handler
@@ -421,6 +400,26 @@ var app = {
         alert('Vous êtes hors connexion ! Ressayez plus tard.');
         navigator.app.exitApp();
     },
+
+    //on gére ici tout les "backbutton"
+    onBackButton: function() {
+        console.log($.mobile.activePage.attr('id'));
+        if ($.mobile.activePage.attr('id')=='page1'){
+            navigator.app.exitApp();
+        }
+        else if ($.mobile.activePage.attr('id')=='page2'){
+            $.mobile.changePage('#page1');
+            location.reload();
+        }
+        else if ($.mobile.activePage.attr('id')=='page3'){
+            outils.effacerTableau();
+            $.mobile.changePage('#page2');
+        }
+        else if ($.mobile.activePage.attr('id')=='page4'){
+            //pour gérer le fait que un backbutton sur la page4 peut revenir soit a la page 1 soit a la page 3
+            window.history.back();
+        }
+    }
 
 };
 
